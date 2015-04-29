a44011ae29d47b56e801a41a6f080d771c2f3998
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2015-04-29 17:24:02 +0100

    Add DataAPIClient and tests

--- dmutils/apiclient.py
+++ dmutils/apiclient.py
@@ -6,6 +6,19 @@ import logging
 logger = logging.getLogger(__name__)
 
 
+class APIError(StandardError):
+    def __init__(self, message, requests_error):
+        super(APIError, self).__init__(message)
+        self.requests_error = requests_error
+
+    @property
+    def response_message(self):
+        try:
+            return self.requests_error.response.json()['error']
+        except TypeError:
+            return str(self.requests_error.response.content)
+
+
 class BaseAPIClient(object):
     def __init__(self, base_url=None, auth_token=None, enabled=None):
         self.base_url = base_url
@@ -15,22 +28,28 @@ class BaseAPIClient(object):
     def _put(self, url, data):
         return self._request("PUT", url, data=data)
 
-    def _get(self, url, params):
+    def _get(self, url, params=None):
         return self._request("GET", url, params=params)
+
+    def _post(self, url, data):
+        return self._request("POST", url, data=data)
 
     def _request(self, method, url, data=None, params=None):
         if self.enabled:
-            logger.debug("API request %s %s", method, url)
-            headers = {
-                "Content-type": "application/json",
-                "Authorization": "Bearer {}".format(self.auth_token),
-            }
-            response = requests.request(
-                method, url,
-                headers=headers, data=data, params=params)
-            response.raise_for_status()
+            try:
+                logger.debug("API request %s %s", method, url)
+                headers = {
+                    "Content-type": "application/json",
+                    "Authorization": "Bearer {}".format(self.auth_token),
+                }
+                response = requests.request(
+                    method, url,
+                    headers=headers, data=data, params=params)
+                response.raise_for_status()
 
-            return response.json()
+                return response.json()
+            except requests.HTTPError as e:
+                raise APIError(e.message, e)
 
 
 class SearchAPIClient(BaseAPIClient):
@@ -56,7 +75,7 @@ class SearchAPIClient(BaseAPIClient):
         url = self._url("/{}".format(service_id))
         data = self._convert_service(service_id, service, supplier_name)
 
-        return self._put(url, data)
+        return self._put(url, data=data)
 
     def _convert_service(self, service_id, service, supplier_name):
         data = {k: service[k] for k in self.FIELDS if k in service}
@@ -66,3 +85,35 @@ class SearchAPIClient(BaseAPIClient):
         return {
             "service": data
         }
+
+
+class DataAPIClient(BaseAPIClient):
+    def init_app(self, app):
+        self.base_url = app.config['DM_DATA_API_URL']
+        self.auth_token = app.config['DM_DATA_API_AUTH_TOKEN']
+
+    def get_service(self, service_id):
+        return self._get(
+            "{}/services/{}".format(self.base_url, service_id))['services']
+
+    def find_service(self, supplier_id=None, page=None):
+        params = {}
+        if supplier_id is not None:
+            params['supplier_id'] = supplier_id
+        if page is not None:
+            params['page'] = page
+
+        return self._get(
+            self.base_url + "/services",
+            params=params)['services']
+
+    def update_service(self, service_id, service, user, reason):
+        return self._post(
+            "{}/services/{}".format(self.base_url, service_id),
+            data={
+                "update_details": {
+                    "updated_by": user,
+                    "update_reason": reason,
+                },
+                "services": service,
+            })
