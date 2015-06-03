35df8aeba6ba8a4f830de982aefb7e82c92e2662
Paul Craig <paul.craig@digital.cabinet-office.gov.uk>
2015-06-03 14:17:40 +0100

    Harmonize create_app stuff and get_version_label()
    Pull out some of the generalized configuration for each app.
    Also pull out the get_version_label` method from the
    status utils.py file that each app implements uniformly.

--- dmutils/__init__.py
+++ dmutils/__init__.py
@@ -1,1 +1,38 @@
-__version__ = '0.16.0'
+from . import logging, config, proxy_fix
+from flask_featureflags.contrib.inline import InlineFeatureFlag
+
+__version__ = '0.17.0'
+
+def init_app(
+        application,
+        config_object,
+        bootstrap=None,
+        data_api_client=None,
+        feature_flags=None,
+        login_manager=None,
+        search_api_client=None,
+):
+
+    application.config.from_object(config_object)
+    config_object.init_app(application)
+
+    # all belong to dmutils
+    config.init_app(application)
+    logging.init_app(application)
+    proxy_fix.init_app(application)
+
+    if bootstrap:
+        bootstrap.init_app(application)
+    if data_api_client:
+        data_api_client.init_app(application)
+    if feature_flags:
+        feature_flags.init_app(application)
+        feature_flags.clear_handlers()
+        feature_flags.add_handler(InlineFeatureFlag())
+    if login_manager:
+        login_manager.init_app(application)
+    if search_api_client:
+        search_api_client.init_app(application)
+
+    logging.init_app(application)
+    proxy_fix.init_app(application)
