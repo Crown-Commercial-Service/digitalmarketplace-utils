512b37e9309d20a853775740ae25ea54b6e07bd6
Martyn Inglis <martyn.inglis@digital.cabinet-office.gov.uk>
2015-06-11 10:19:30 +0100

    Added more audit cases

--- dmutils/audit.py
+++ dmutils/audit.py
@@ -6,6 +6,15 @@ class AuditTypes(Enum):
 
     contact_update = "contact_update"
     supplier_update = "supplier_update"
+    create_draft_service = "create_draft_service"
+    update_draft_service = "update_draft_service"
+    publish_draft_service = "publish_draft_service"
+    update_service = "update_service"
+    import_service = "import_service"
+    update_service_status = "update_service_status"
+    user_auth_failed = "user_auth_failed"
+    create_user = "create_user"
+    update_user = "update_user"
 
     @staticmethod
     def is_valid_audit_type(test_audit_type):
