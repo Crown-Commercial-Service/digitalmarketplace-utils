e02fec1af8b99f7fc0fd2fd20f10d2b2f7f798f6
Chris Hill-Scott <me@quis.cc>
2015-06-22 15:18:36 +0100

    Bump version to 2.0.0
    Includes
    - #74 Add .get_next_section_id() method to content loader 
    
    This is a MAJOR version because it changes the interface to content_loader

--- dmutils/__init__.py
+++ dmutils/__init__.py
@@ -2,7 +2,7 @@ from . import logging, config, proxy_fix
 import flask_featureflags
 from flask_featureflags.contrib.inline import InlineFeatureFlag
 
-__version__ = '1.0.6'
+__version__ = '2.0.0'
 
 
 def init_app(
