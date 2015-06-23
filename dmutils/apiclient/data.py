6b921d03f1ce29939bdfebf5c3be43fbe7ce7b0e
Paul Craig <paul.craig@digital.cabinet-office.gov.uk>
2015-06-23 14:13:16 +0100

    Group routes in the data api by implicated models
    Thought it was confusing while looking for a specific route, so
    I've grouped them roughly by dependencies.
    Audit Events -> Suppliers -> Users -> Services

--- dmutils/apiclient/data.py
+++ dmutils/apiclient/data.py
@@ -7,6 +7,8 @@ class DataAPIClient(BaseAPIClient):
         self.base_url = app.config['DM_DATA_API_URL']
         self.auth_token = app.config['DM_DATA_API_AUTH_TOKEN']
 
+    # Audit Events
+
     def find_audit_events(
             self,
             audit_type=None,
@@ -37,6 +39,113 @@ class DataAPIClient(BaseAPIClient):
                     "updated_by": user
                 }
             })
+
+    # Suppliers
+
+    def find_suppliers(self, prefix=None, page=None):
+        params = {}
+        if prefix:
+            params["prefix"] = prefix
+        if page is not None:
+            params['page'] = page
+
+        return self._get(
+            "/suppliers",
+            params=params
+        )
+
+    def get_supplier(self, supplier_id):
+        return self._get(
+            "/suppliers/{}".format(supplier_id)
+        )
+
+    def create_supplier(self, supplier_id, supplier):
+        return self._put(
+            "/suppliers/{}".format(supplier_id),
+            data={"suppliers": supplier},
+        )
+
+    def update_supplier(self, supplier_id, supplier, user):
+        return self._post(
+            "/suppliers/{}".format(supplier_id),
+            data={
+                "suppliers": supplier,
+                "updated_by": user,
+            },
+        )
+
+    def update_contact_information(self, supplier_id, contact_id,
+                                   contact, user):
+        return self._post(
+            "/suppliers/{}/contact-information/{}".format(
+                supplier_id, contact_id),
+            data={
+                "contactInformation": contact,
+                "updated_by": user,
+            },
+        )
+
+    # Users
+
+    def create_user(self, user):
+        return self._post(
+            "/users",
+            data={
+                "users": user,
+            })
+
+    def get_user(self, user_id=None, email_address=None):
+        if user_id is not None and email_address is not None:
+            raise ValueError(
+                "Cannot get user by both user_id and email_address")
+        elif user_id is not None:
+            url = "{}/users/{}".format(self.base_url, user_id)
+            params = {}
+        elif email_address is not None:
+            url = "{}/users".format(self.base_url)
+            params = {"email": email_address}
+        else:
+            raise ValueError("Either user_id or email_address must be set")
+
+        try:
+            return self._get(url, params=params)
+        except HTTPError as e:
+            if e.status_code != 404:
+                raise
+        return None
+
+    def authenticate_user(self, email_address, password, supplier=True):
+        try:
+            response = self._post(
+                '/users/auth',
+                data={
+                    "authUsers": {
+                        "emailAddress": email_address,
+                        "password": password,
+                    }
+                })
+            if not supplier or "supplier" in response['users']:
+                return response
+        except HTTPError as e:
+            if e.status_code not in [400, 403, 404]:
+                raise
+        return None
+
+    def update_user_password(self, user_id, new_password):
+        try:
+            self._post(
+                '/users/{}'.format(user_id),
+                data={"users": {"password": new_password}}
+            )
+
+            logger.info("Updated password for user %s", user_id)
+            return True
+        except HTTPError as e:
+            logger.info("Password update failed for user %s: %s",
+                        user_id, e.status_code)
+            return False
+
+    # Services
 
     def create_new_draft_service(self, framework_slug, supplier_id, user, lot):
         return self._post(
@@ -103,49 +212,6 @@ class DataAPIClient(BaseAPIClient):
                 },
             })
 
-    def find_suppliers(self, prefix=None, page=None):
-        params = {}
-        if prefix:
-            params["prefix"] = prefix
-        if page is not None:
-            params['page'] = page
-
-        return self._get(
-            "/suppliers",
-            params=params
-        )
-
-    def get_supplier(self, supplier_id):
-        return self._get(
-            "/suppliers/{}".format(supplier_id)
-        )
-
-    def create_supplier(self, supplier_id, supplier):
-        return self._put(
-            "/suppliers/{}".format(supplier_id),
-            data={"suppliers": supplier},
-        )
-
-    def update_supplier(self, supplier_id, supplier, user):
-        return self._post(
-            "/suppliers/{}".format(supplier_id),
-            data={
-                "suppliers": supplier,
-                "updated_by": user,
-            },
-        )
-
-    def update_contact_information(self, supplier_id, contact_id,
-                                   contact, user):
-        return self._post(
-            "/suppliers/{}/contact-information/{}".format(
-                supplier_id, contact_id),
-            data={
-                "contactInformation": contact,
-                "updated_by": user,
-            },
-        )
-
     def get_archived_service(self, archived_service_id):
         return self._get("/archived-services/{}".format(archived_service_id))
 
@@ -200,61 +266,3 @@ class DataAPIClient(BaseAPIClient):
                     "update_reason": reason,
                 },
             })
-
-    def create_user(self, user):
-        return self._post(
-            "/users",
-            data={
-                "users": user,
-            })
-
-    def get_user(self, user_id=None, email_address=None):
-        if user_id is not None and email_address is not None:
-            raise ValueError(
-                "Cannot get user by both user_id and email_address")
-        elif user_id is not None:
-            url = "{}/users/{}".format(self.base_url, user_id)
-            params = {}
-        elif email_address is not None:
-            url = "{}/users".format(self.base_url)
-            params = {"email": email_address}
-        else:
-            raise ValueError("Either user_id or email_address must be set")
-
-        try:
-            return self._get(url, params=params)
-        except HTTPError as e:
-            if e.status_code != 404:
-                raise
-        return None
-
-    def authenticate_user(self, email_address, password, supplier=True):
-        try:
-            response = self._post(
-                '/users/auth',
-                data={
-                    "authUsers": {
-                        "emailAddress": email_address,
-                        "password": password,
-                    }
-                })
-            if not supplier or "supplier" in response['users']:
-                return response
-        except HTTPError as e:
-            if e.status_code not in [400, 403, 404]:
-                raise
-        return None
-
-    def update_user_password(self, user_id, new_password):
-        try:
-            self._post(
-                '/users/{}'.format(user_id),
-                data={"users": {"password": new_password}}
-            )
-
-            logger.info("Updated password for user %s", user_id)
-            return True
-        except HTTPError as e:
-            logger.info("Password update failed for user %s: %s",
-                        user_id, e.status_code)
-            return False
