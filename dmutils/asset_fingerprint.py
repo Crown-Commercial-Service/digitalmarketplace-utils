b31df0316b8a98b7f60273a8fada731c08116005
Chris Hill-Scott <me@quis.cc>
2015-07-27 09:00:13 +0100

    Generate URLs within asset fingerprinter util
    We need to generate URLs for about 6 asset files now, which would mean quite a
    lot of repetition in each apps templates and `Config.py`s.
    
    This commit makes 'asset fingerprinter' into a class, and gives it a method to
    return the path and querystring for an asset, rather than just the hash.
    
    Usage
    
    `Config.py`:
    ```
    config.asset_fingerprinter = AssetFingerprinter(
      asset_root='/suppliers/static/'
    )
    ```
    
    `_base_template.html`:
    ```
    {{ asset_fingerprinter.get_url('stylesheets/application.css') }}
    ```
    
    Result:
    ```
    /suppliers/static/stylesheets/application.css?418e6f4a6cdf1142e45c072ed3e1c90a
    ```
    """

--- dmutils/asset_fingerprint.py
+++ dmutils/asset_fingerprint.py
@@ -1,20 +1,45 @@
 import hashlib
 
 
-def get_asset_fingerprint(asset_file_path):
+class AssetFingerprinter():
     """
         Get a unique hash for an asset file, so that it doesn't stay cached
         when it changes
 
         Usage:
-        get('app/static/stylesheets/application.css')
+
+            Config.py:
+            base_template_data.asset_fingerprinter = AssetFingerprinter(
+                asset_root='/suppliers/static/'
+            )
+
+            _base_template.html:
+            {{ asset_fingerprinter.get_url('stylesheets/application.css') }}
+
+        * 'app/static' is assumed to be the root for all asset files
     """
-    return hashlib.md5().update(
-        get_asset_file_contents(asset_file_path)
-    ).hexdigest()
 
+    def __init__(self, asset_root='/static/', filesystem_path='app/static/'):
+        self._cache = {}
+        self._asset_root = asset_root
+        self._filesystem_path = filesystem_path
 
-def get_asset_file_contents(asset_file_path):
-    with open(asset_file_path, 'rb') as asset_file:
-        contents = asset_file.read()
-    return contents
+    def get_url(self, asset_path):
+        if asset_path not in self._cache:
+            self._cache[asset_path] = (
+                self._asset_root +
+                asset_path +
+                '?' +
+                self.get_asset_fingerprint(self._filesystem_path + asset_path)
+            )
+        return self._cache[asset_path]
+
+    def get_asset_fingerprint(self, asset_file_path):
+        return hashlib.md5(
+            self.get_asset_file_contents(asset_file_path).encode('utf-8')
+        ).hexdigest()
+
+    def get_asset_file_contents(self, asset_file_paths):
+        with open(asset_file_path, 'rb') as asset_file:
+            contents = asset_file.read()
+        return contents
