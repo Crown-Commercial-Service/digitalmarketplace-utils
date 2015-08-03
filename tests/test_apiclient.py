99631b36b440775a63cf8fe6bf28e6ce235f4cbe
Alexey Bezhan <al@fueledbylemons.com>
2015-08-03 14:19:34 +0100

    Send draft update page_questions outside service data
    Adds additional argument to draft update method to pass
    `page_questions`.
    
    This way, `page_questions` is more explicit as a meta field and is
    not mixed with the service data.

--- tests/test_apiclient.py
+++ tests/test_apiclient.py
@@ -861,7 +861,30 @@ class TestDataApiClient(object):
             },
             'update_details': {
                 'update_reason': 'deprecated', 'updated_by': 'user'
-            }
+            },
+        }
+
+    def test_update_draft_service_with_page_questions(self, data_client, rmock):
+        rmock.post(
+            "http://baseurl/draft-services/2",
+            json={"done": "it"},
+            status_code=200,
+        )
+
+        result = data_client.update_draft_service(
+            2, {"field": "value"}, 'user', ['question1', 'question2']
+        )
+
+        assert result == {"done": "it"}
+        assert rmock.called
+        assert rmock.request_history[0].json() == {
+            'services': {
+                "field": "value"
+            },
+            'update_details': {
+                'update_reason': 'deprecated', 'updated_by': 'user'
+            },
+            'page_questions': ['question1', 'question2']
         }
 
     def test_publish_draft_service(self, data_client, rmock):
