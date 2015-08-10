2641ae009950876f634254aca0923ae09df7153c
Martyn Inglis <martyn.inglis@digital.cabinet-office.gov.uk>
2015-08-10 09:16:18 +0100

    Bumped version for log message changes

--- dmutils/__init__.py
+++ dmutils/__init__.py
@@ -3,7 +3,7 @@ from datetime import datetime
 import flask_featureflags
 from flask_featureflags.contrib.inline import InlineFeatureFlag
 
-__version__ = '4.5.0'
+__version__ = '4.5.1'
 
 
 def init_app(
