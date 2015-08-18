f0ca280e3291864f28aafaf1d79a8c0d15b29bf3
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2015-08-18 15:45:07 +0100

    Bump major version
    Forcing AuditTypes to be used for data API client arguments is a
    breaking change.

--- dmutils/__init__.py
+++ dmutils/__init__.py
@@ -3,7 +3,7 @@ from datetime import datetime
 import flask_featureflags
 from flask_featureflags.contrib.inline import InlineFeatureFlag
 
-__version__ = '5.4.0'
+__version__ = '6.0.0'
 
 
 def init_app(
