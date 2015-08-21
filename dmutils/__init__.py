de3cc45b841e723872d6760c2da90e8972a43ca2
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2015-08-21 14:25:16 +0100

    Bump version to 6.2.1

--- dmutils/__init__.py
+++ dmutils/__init__.py
@@ -3,7 +3,7 @@ from datetime import datetime
 import flask_featureflags
 from flask_featureflags.contrib.inline import InlineFeatureFlag
 
-__version__ = '6.2.0'
+__version__ = '6.2.1'
 
 
 def init_app(
