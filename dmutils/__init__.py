eb6129a9f0d4fe3dd30d93da7be9048bad6dadd9
Paul Craig <paul.craig@digital.cabinet-office.gov.uk>
2015-08-27 13:48:51 +0100

    Add dayformat template filter
    Forgot to do this earlier.  Gives the templates a 'DD Month'
    display date filter (for example '27 August').

--- dmutils/__init__.py
+++ dmutils/__init__.py
@@ -48,5 +48,6 @@ def init_app(
         return response
 
     application.add_template_filter(formats.timeformat)
+    application.add_template_filter(formats.dayformat)
     application.add_template_filter(formats.dateformat)
     application.add_template_filter(formats.datetimeformat)
