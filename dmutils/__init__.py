feb631586f4a68a32f88dd8384951a8b43281258
Kev Keenoy <kevin.keenoy@digital.cabinet-office.gov.uk>
2015-08-28 10:32:22 +0100

    Bump version to 6.7.0
    Minor version change as non-breaking extra functionality.

--- dmutils/__init__.py
+++ dmutils/__init__.py
@@ -3,7 +3,7 @@ from datetime import datetime
 import flask_featureflags
 from flask_featureflags.contrib.inline import InlineFeatureFlag
 
-__version__ = '6.6.0'
+__version__ = '6.7.0'
 
 
 def init_app(
