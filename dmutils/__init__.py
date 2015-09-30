5676a0848195591476da4e39ecb60a37dbff6933
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2015-09-30 15:09:21 +0100

    Bump version to 8.2.0

--- dmutils/__init__.py
+++ dmutils/__init__.py
@@ -3,7 +3,7 @@ from datetime import datetime
 import flask_featureflags
 from flask_featureflags.contrib.inline import InlineFeatureFlag
 
-__version__ = '8.1.2'
+__version__ = '8.2.0'
 
 
 def init_app(
