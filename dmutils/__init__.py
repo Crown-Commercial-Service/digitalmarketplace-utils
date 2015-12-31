bd5b278090296f6dc4b9043d391732a9a8cd3f01
Alexey Bezhan <al@fueledbylemons.com>
2015-12-31 14:03:14 +0000

    Remove apiclient and audit modules in favour of dmapiclient
    apiclient and audit were moved to the new dmapiclient package, so
    local modules can be removed.
    
    `dmutils.apiclient` and `dmutils.audit` import paths are preserved
    for package-level names to simplify transition, but imports from
    apiclient modules (eg `from dmutils.apiclient.errors ....`) will
    have to be changed.

--- dmutils/__init__.py
+++ dmutils/__init__.py
@@ -1,6 +1,9 @@
 from . import logging, config, proxy_fix, formats, request_id
 from .flask_init import init_app, init_manager
 
+import dmapiclient as apiclient
+from dmapiclient import audit
+
 import flask_featureflags
 
 __version__ = '15.12.0'
