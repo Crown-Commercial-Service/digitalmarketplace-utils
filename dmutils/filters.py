6dd8e202333f997e19b345523978173971283a0c
Paul Craig <paul_craig_16@hotmail.com>
2016-04-19 17:21:31 +0100

    Extend markdown less
    Only use the `abbr` extension.
    `extra` includes 6 other extensions, none of which we need
    right now.

--- dmutils/filters.py
+++ dmutils/filters.py
@@ -2,4 +2,4 @@ from markdown import markdown
 
 
 def markdown_filter(text, *args, **kwargs):
-    return markdown(text, ['markdown.extensions.extra'], *args, **kwargs)
+    return markdown(text, ['markdown.extensions.abbr'], *args, **kwargs)
