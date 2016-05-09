0e47160abf8b956f255cd416137a6836934a7626
Chris Wynne <christopher.wynne@digital.cabinet-office.gov.uk>
2016-05-09 17:09:10 +0100

    Remove format functions and tests
    The format_price function in dmutils.formats was only used by content
    loader so it has been moved to the new package.
    
    The format_service_price function is dependent on format_price so has
    been moved too. It is however used by the front end apps - they will
    need updating to import it from the new location.
    
    The relevant tests have been moved to the new package.

--- dmutils/formats.py
+++ dmutils/formats.py
@@ -77,37 +77,3 @@ def get_label_for_lot_param(lot_to_check):
     if lot_i_found:
         return lot_i_found[0]['label']
     return None
-
-
-def format_service_price(service):
-    """Format a price string from a service dictionary
-
-    :param service: a service dictionary, returned from data API
-
-    :return: a formatted price string if the required
-             fields are set in the service dictionary.
-    """
-    if not service.get('priceMin'):
-        return ''
-    return format_price(
-        service.get('priceMin'),
-        service.get('priceMax'),
-        service.get('priceUnit'),
-        service.get('priceInterval'))
-
-
-def format_price(min_price, max_price, unit, interval, hours_for_price=None):
-    """Format a price string"""
-    if hours_for_price:
-        return u'{} for £{}'.format(hours_for_price, min_price)
-
-    if min_price is None:
-        raise TypeError('min_price should be string or integer, not None')
-    formatted_price = u'£{}'.format(min_price)
-    if max_price:
-        formatted_price += u' to £{}'.format(max_price)
-    if unit:
-        formatted_price += ' per ' + unit.lower()
-    if interval:
-        formatted_price += ' per ' + interval.lower()
-    return formatted_price
--- tests/test_formats.py
+++ tests/test_formats.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from dmutils.formats import (
     get_label_for_lot_param, lot_to_lot_case,
-    format_price, format_service_price,
     timeformat, shortdateformat, dateformat, datetimeformat
 )
 import pytz
@@ -36,59 +35,6 @@ class TestFormats(object):
 
         for example, expected in cases:
             assert get_label_for_lot_param(example) == expected
-
-
-def test_format_service_price():
-    service = {
-        'priceMin': '12.12',
-        'priceMax': '13.13',
-        'priceUnit': 'Unit',
-        'priceInterval': 'Second',
-    }
-
-    cases = [
-        ('12.12', u'£12.12 to £13.13 per unit per second'),
-        ('', ''),
-        (None, ''),
-    ]
-
-    def check_service_price_formatting(price_min, formatted_price):
-        service['priceMin'] = price_min
-        assert format_service_price(service) == formatted_price
-
-    for price_min, formatted_price in cases:
-        yield check_service_price_formatting, price_min, formatted_price
-
-
-def test_format_price():
-    cases = [
-        ((u'12', None, 'Unit', None), u'£12 per unit'),
-        (('12', '13', 'Unit', None), u'£12 to £13 per unit'),
-        (('12', '13', 'Unit', 'Second'), u'£12 to £13 per unit per second'),
-        (('12', None, 'Unit', 'Second'), u'£12 per unit per second'),
-        ((12, 13, 'Unit', None), u'£12 to £13 per unit'),
-        (('34', None, 'Lab', None, '4 hours'), u'4 hours for £34'),
-        (('12', None, None, None), u'£12'),
-    ]
-
-    def check_price_formatting(args, formatted_price):
-        assert format_price(*args) == formatted_price
-
-    for args, formatted_price in cases:
-        yield check_price_formatting, args, formatted_price
-
-
-def test_format_price_errors():
-    cases = [
-        (None, None, None, None),
-    ]
-
-    def check_price_formatting(case):
-        with pytest.raises((TypeError, AttributeError)):
-            format_price(*case)
-
-    for case in cases:
-        yield check_price_formatting, case
 
 
 def test_timeformat():
