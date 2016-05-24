45ed46c525acf3873cad31dc54c8e8752c2de016
Chris Wynne <christopher.wynne@digital.cabinet-office.gov.uk>
2016-05-24 11:58:05 +0100

    Add smartjoin filter and tests
    Add a new filter for properly joining and formatting a list of strings.

--- dmutils/filters.py
+++ dmutils/filters.py
@@ -3,3 +3,12 @@ from markdown import markdown
 
 def markdown_filter(text, *args, **kwargs):
     return markdown(text, ['markdown.extensions.abbr'], *args, **kwargs)
+
+
+def smartjoin(list_to_join):
+    if len(list_to_join) > 1:
+        return '{} and {}'.format(', '.join(list_to_join[:-1]), list_to_join[-1])
+    elif len(list_to_join) == 1:
+        return '{}'.format(list_to_join[0])
+    else:
+        return ''
