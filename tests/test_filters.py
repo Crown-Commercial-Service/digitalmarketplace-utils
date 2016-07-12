c2a45bfd647f84b8e90cbaad3fe8c325af4c6dfd
Robert Scott <robert.scott@digital.cabinet-office.gov.uk>
2016-07-12 11:51:55 +0100

    format_links: apply specific (but customizable) classes to inserted html elements

--- tests/test_filters.py
+++ tests/test_filters.py
@@ -52,25 +52,25 @@ def test_smartjoin_for_empty_list():
 
 def test_format_link():
     link = 'http://www.example.com'
-    formatted_link = '<a href="http://www.example.com" rel="external">http://www.example.com</a>'
+    formatted_link = '<a href="http://www.example.com" class="urlized-link" rel="external">http://www.example.com</a>'
     assert format_links(link) == formatted_link
 
 
 def test_format_link_without_protocol():
     link = 'www.example.com'
-    formatted_link = '<span class="break-link">www.example.com</span>'
+    formatted_link = '<span class="urlized-plaintext-link">www.example.com</span>'
     assert format_links(link) == formatted_link
 
 
 def test_format_link_with_text():
     text = 'This is the Greek Γ Δ Ε Ζ Η Θ Ι Κ Λ link: http://www.exΔmple.com'
-    formatted_text = 'This is the Greek Γ Δ Ε Ζ Η Θ Ι Κ Λ link: <a href="http://www.exΔmple.com" rel="external">http://www.exΔmple.com</a>'  # noqa
+    formatted_text = 'This is the Greek Γ Δ Ε Ζ Η Θ Ι Κ Λ link: <a href="http://www.exΔmple.com" class="urlized-link" rel="external">http://www.exΔmple.com</a>'  # noqa
     assert format_links(text) == formatted_text
 
 
 def test_format_link_and_text_escapes_extra_html():
     text = 'This is the <strong>link</strong>: http://www.example.com'
-    formatted_text = 'This is the &lt;strong&gt;link&lt;/strong&gt;: <a href="http://www.example.com" rel="external">http://www.example.com</a>'  # noqa
+    formatted_text = 'This is the &lt;strong&gt;link&lt;/strong&gt;: <a href="http://www.example.com" class="urlized-link" rel="external">http://www.example.com</a>'  # noqa
     assert format_links(text) == formatted_text
 
 
@@ -78,17 +78,26 @@ def test_format_link_does_not_die_horribly():
     text = 'This is the URL that made a previous regex die horribly' \
            'https://something&lt;span&gt;what&lt;/span&gt;something.com'
     formatted_text = 'This is the URL that made a previous regex die horribly' \
-                     '<a href="https://something&amp;lt;span&amp;gt;what&amp;lt;/span&amp;gt;something.com" ' \
-                     'rel="external">https://something&amp;lt;span&amp;gt;what&amp;lt;/span&amp;gt;something.com</a>'
+                     '<a href="https://something&amp;lt;span&amp;gt;what&amp;lt;/span&amp;gt;something.com"' \
+                     ' class="urlized-link" rel="external">https://something&amp;lt;span&amp;gt;what&amp;lt;/span'\
+                     '&amp;gt;something.com</a>'
     assert format_links(text) == formatted_text
 
 
 def test_multiple_urls():
     text = 'This is the first link http://www.example.com and this is the second http://secondexample.com.'  # noqa
-    formatted_text = 'This is the first link <a href="http://www.example.com" '\
+    formatted_text = 'This is the first link <a href="http://www.example.com" class="urlized-link" '\
         'rel="external">http://www.example.com</a> and this is the second '\
-        '<a href="http://secondexample.com" rel="external">http://secondexample.com</a>.'
+        '<a href="http://secondexample.com" class="urlized-link" rel="external">http://secondexample.com</a>.'
     assert format_links(text) == formatted_text
+
+
+def test_multiple_urls_alternate_classes():
+    text = 'This is the first link www.example.com. This is the second http://secondexample.com...'  # noqa
+    formatted_text = 'This is the first link '\
+        '<span class="four">www.example.com</span>. This is the second '\
+        '<a href="http://secondexample.com" class="one two-three" rel="external">http://secondexample.com</a>...'
+    assert format_links(text, link_classes=("one", "two-three",), plaintext_link_classes=("four",)) == formatted_text
 
 
 def test_no_links_no_change():
