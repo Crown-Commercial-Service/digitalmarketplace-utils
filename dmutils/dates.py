2b78d709529d06a0ce81877f9bc1a9ae8152ed1c
Chris Wynne <christopher.wynne@digital.cabinet-office.gov.uk>
2016-07-25 11:05:19 +0100

    Add dates package
    The dates package contains the logic for working out publishing dates
    for briefs, and is currenty used by the buyer frontend and the api.
    
    The way the dates are calculated is slightly different depending on if
    the get_publishing_dates function is used by the api or the frontend due
    to the way that briefs are serialized when returned to the frontend.
    
    Working days are taken into account via the workdays package brought in
    with pip.

---
+++ dmutils/dates.py
@@ -0,0 +1,54 @@
+from six import string_types
+from datetime import datetime, timedelta
+from workdays import workday
+
+
+def get_publishing_dates(brief):
+    APPLICATION_OPEN_DAYS = {'1 week': 7, '2 weeks': 14}
+    QUESTIONS_OPEN_DAYS = {'1 week': 2, '2 weeks': 5}
+    dates = {}
+
+    if brief_is_from_api_(brief):
+        dates['today'] = get_start_date_from_brief_api(brief)
+    elif brief_is_from_frontend_app_and_published_(brief):
+        dates['today'] = get_start_date_from_published_frontend_brief(brief)
+    else:
+        dates['today'] = get_todays_date()
+
+    length = get_length_of_brief(brief)
+
+    dates['closing_date'] = dates['today'] + timedelta(days=APPLICATION_OPEN_DAYS[length])
+    dates['questions_close'] = workday(dates['today'], QUESTIONS_OPEN_DAYS[length])
+    dates['answers_close'] = workday(dates['closing_date'], -1)
+    dates['application_open_weeks'] = APPLICATION_OPEN_DAYS[length]//7
+    dates['closing_time'] = '{d:%I:%M %p}'.format(d=dates['closing_date']).lower()
+
+    return dates
+
+
+def brief_is_from_api_(brief):
+    return brief.get('publishedAt') and isinstance(brief.get('publishedAt'), datetime)
+
+
+def brief_is_from_frontend_app_and_published_(brief):
+    return brief.get('publishedAt') and isinstance(brief.get('publishedAt'), string_types)
+
+
+def get_start_date_from_brief_api(brief):
+    return brief.get('publishedAt').replace(hour=23, minute=59, second=59, microsecond=0)
+
+
+def get_start_date_from_published_frontend_brief(brief):
+    return datetime.strptime(brief['publishedAt'][0:10], '%Y-%m-%d') \
+        .replace(hour=23, minute=59, second=59, microsecond=0)
+
+
+def get_todays_date():
+    return datetime.utcnow().replace(hour=23, minute=59, second=59, microsecond=0)
+
+
+def get_length_of_brief(brief):
+    if brief.get('requirementsLength') == '1 week':
+        return '1 week'
+    else:
+        return '2 weeks'
