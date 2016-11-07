e617e2d2bd1b71a41c6f081d79b839b03ee90e3f
Paul Craig <paul_craig_16@hotmail.com>
2016-11-07 10:55:58 +0000

    Remove markdown from utils
    This removes the markdown filter from our jinja templates going
    forward, so it's a breaking change. 💔

--- tests/test_filters.py
+++ tests/test_filters.py
@@ -1,35 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from dmutils.filters import markdown_filter, smartjoin, format_links
-
-
-def test_markdown_filter_produces_markup():
-
-    markdown_string = """## H2 title
-
-- List item 1
-- List item 2
-
-Paragraph
-**Bold**
-*Emphasis*
-
-HTML is an abbreviation.
-
-*[HTML]: Hyper Text Markup Language
-"""
-
-    html_string = """<h2>H2 title</h2>
-<ul>
-<li>List item 1</li>
-<li>List item 2</li>
-</ul>
-<p>Paragraph
-<strong>Bold</strong>
-<em>Emphasis</em></p>
-<p><abbr title="Hyper Text Markup Language">HTML</abbr> is an abbreviation.</p>"""
-
-    assert markdown_filter(markdown_string) == html_string
+from dmutils.filters import smartjoin, format_links
 
 
 def test_smartjoin_for_more_than_one_item():
