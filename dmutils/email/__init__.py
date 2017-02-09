0edcf58bef18db3c9fb3bfb1fa424d02fd1156b3
benvand <benjaminfvandersteen@gmail.com>
2017-02-09 17:52:50 +0000

    Repair email method imports bugfix, version bump

--- dmutils/email/__init__.py
+++ dmutils/email/__init__.py
@@ -1,2 +1,4 @@
 """Allow accessing send email from email. Backwards compatibility."""
-from dmutils.email.dm_mandrill import send_email
+from dmutils.email.dm_mandrill import (
+    send_email, generate_token, decode_invitation_token, decode_password_reset_token
+)
