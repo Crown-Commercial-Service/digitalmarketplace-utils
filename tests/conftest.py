f403414d4b97342e4bf10c673751bb236bebb46b
Chris Wynne <christopher.wynne@digital.cabinet-office.gov.uk>
2017-05-08 14:48:30 +0100

    Update logging for PaaS environment
    When we move to PaaS we want to log json format to stdout. We also need,
    in the short term, to log json to file in the Elastic Beanstalk
    environment. This is determined by the `DM_LOG_PATH` env variable.
    
    We're also dropping logging both plain text and json logs in production.
    Be default we'll use json format, but this can be overwritten by setting
    the `DM_PLAIN_TEXT_LOGS` variaable to `True` in the app config.
    
    This will only be done in the dev environment, as seeing the plain text
    logs would be more useful than json.

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,15 +10,9 @@ from dmutils.logging import init_app
 
 @pytest.fixture
 def app():
-    return Flask(__name__)
-
-
-@pytest.yield_fixture
-def app_with_logging(app):
-    with tempfile.NamedTemporaryFile() as f:
-        app.config['DM_LOG_PATH'] = f.name
-        init_app(app)
-        yield app
+    app = Flask(__name__)
+    init_app(app)
+    return app
 
 
 @pytest.yield_fixture
