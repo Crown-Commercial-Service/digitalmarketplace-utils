76a8872664a602958844302cb852ac2003f6bcc8
Paul Craig <paul_craig_16@hotmail.com>
2017-06-29 11:13:50 +0100

    Stop using `bool` as variable name
    `bool` is a python builtin thing so we don't want to redefine it.

--- tests/test_filters.py
+++ tests/test_filters.py
@@ -123,17 +123,18 @@ def test_capitalize_first_for_non_strings():
     assert capitalize_first('https://www.example.com') == 'https://www.example.com'
 
 
-@pytest.mark.parametrize("bool", (False, True,))  # Shouldn't matter
-def test_preserve_line_breaks(bool):
-    assert preserve_line_breaks(mock.Mock(autoescape=bool), '\r\n', 'textbox_large') == '<br>'
-    assert preserve_line_breaks(mock.Mock(autoescape=bool), '\r\n\r\n', 'textbox_large') == '<br><br>'
-    assert preserve_line_breaks(mock.Mock(autoescape=bool), '\r\n \r\n \r\n', 'textbox_large') == '<br><br>'
-    assert preserve_line_breaks(mock.Mock(autoescape=bool), '\r\n\r\n\r\n\r\n\r\n\r\n', 'textbox_large') == '<br><br>'
-    assert preserve_line_breaks(mock.Mock(autoescape=bool), '', 'textbox_large') == ''
-    assert preserve_line_breaks(mock.Mock(autoescape=bool), '\r\n<h2>', 'textbox_large') == '<br>&lt;h2&gt;'
-    assert preserve_line_breaks(mock.Mock(autoescape=bool), '\n', 'textbox_large') == '\n'
-    assert preserve_line_breaks(mock.Mock(autoescape=bool), '\r\n', 'not_textbox_large') == '\r\n'
-    assert preserve_line_breaks(mock.Mock(autoescape=bool),
+@pytest.mark.parametrize("_autoescape", (False, True))  # Shouldn't matter
+def test_preserve_line_breaks(_autoescape):
+    eval_ctx_mock = mock.Mock(autoescape=_autoescape)
+    assert preserve_line_breaks(eval_ctx_mock, '\r\n', 'textbox_large') == '<br>'
+    assert preserve_line_breaks(eval_ctx_mock, '\r\n\r\n', 'textbox_large') == '<br><br>'
+    assert preserve_line_breaks(eval_ctx_mock, '\r\n \r\n \r\n', 'textbox_large') == '<br><br>'
+    assert preserve_line_breaks(eval_ctx_mock, '\r\n\r\n\r\n\r\n\r\n\r\n', 'textbox_large') == '<br><br>'
+    assert preserve_line_breaks(eval_ctx_mock, '', 'textbox_large') == ''
+    assert preserve_line_breaks(eval_ctx_mock, '\r\n<h2>', 'textbox_large') == '<br>&lt;h2&gt;'
+    assert preserve_line_breaks(eval_ctx_mock, '\n', 'textbox_large') == '\n'
+    assert preserve_line_breaks(eval_ctx_mock, '\r\n', 'not_textbox_large') == '\r\n'
+    assert preserve_line_breaks(eval_ctx_mock,
                                 'You’ll be eating 🍕', 'textbox_large') == 'You’ll be eating 🍕'
-    assert preserve_line_breaks(mock.Mock(autoescape=bool),
+    assert preserve_line_breaks(eval_ctx_mock,
                                 '\r\n\r\n  \r\n\r\n  \t\v \r\n\r\n', 'textbox_large') == '<br><br>'
