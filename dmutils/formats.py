1afab2cac01e29d7e5eef444f4e0ea606e83c66c
Kat Stevens <katherine.stevens@digital.cabinet-office.gov.uk>
2017-07-14 16:45:11 +0100

    Append GMT to utcdatetimeformat-filtered dates
    We also now force GMT for any time that is not 11.59pm, not just
    for 12.59am times. This should probably be reviewed if we start
    storing non-naive datestamps in the DB.
    
    Added usage comment to utcdatetimeformat filter.

--- dmutils/formats.py
+++ dmutils/formats.py
@@ -31,11 +31,18 @@ def datetimeformat(value, default_value=None, localize=True):
 
 
 def utcdatetimeformat(value, default_value=None):
+    """
+    Use this filter to format deadline timestamps that are stored in the database as
+    23:59:59 (UTC+00), when the localisation would otherwise display the date as 12.59am
+     on the next day (due to daylight savings), potentially causing confusion for buyers/suppliers.
+    """
     local_format = datetimeformat(value, default_value)
-    if local_format and "12:59am" in local_format:
-        # Force UTC+00 if the date has rolled over to the next day
-        return datetimeformat(value, default_value, localize=False)
-    return local_format
+    if local_format:
+        if "11:59pm" not in local_format:
+            # Force UTC+00 if the date has rolled over to the next day and append timezone
+            return "{} GMT".format(datetimeformat(value, default_value, localize=False))
+        return "{} GMT".format(local_format)
+    return None
 
 
 def datetodatetimeformat(value):
