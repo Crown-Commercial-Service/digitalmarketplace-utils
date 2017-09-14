c7d299f206e85353069b67ae11a90478a91b7a5c
Alexey Bezhan <al@fueledbylemons.com>
2017-09-14 16:31:38 +0100

    Move invitation/reset token related functions to a separate module
    Token generation and decryption functions used to be part of dm_mandrill
    module since they were only used for emails sent through the Mandrill
    API. Now that we're switching user registration and password reset
    emails to use Notify it makes sense to move functions to a separate
    module.
    
    This could be a breaking change, but none of the apps are importing
    the functions from the module (instead they use `dmutils.email`).

--- dmutils/email/__init__.py
+++ dmutils/email/__init__.py
@@ -1,6 +1,7 @@
 """Allow accessing send email from email. Backwards compatibility."""
-from dmutils.email.dm_mandrill import (
-    send_email, generate_token, decode_invitation_token, decode_password_reset_token
+from dmutils.email.dm_mandrill import send_email
+from dmutils.email.tokens import (
+    generate_token, decode_invitation_token, decode_password_reset_token
 )
 
 from .exceptions import EmailError
