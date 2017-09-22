2578841aa184240647a2dc7c03ca63959bb085a1
Chris Wynne <christopher.wynne@digital.cabinet-office.gov.uk>
2017-09-22 15:44:42 +0100

    Convert UserAccountEmail class to function
    This changes the logic to a function which can be imported and used in
    the apps.
    
    It also imports the external blueprint which has been moved to utils.
    This means all logic can live in the function instead of having to pass
    in the url as before.

--- dmutils/email/user_account_email.py
+++ dmutils/email/user_account_email.py
@@ -1,40 +1,42 @@
-from flask import current_app, session, abort
+from flask import current_app, session, abort, url_for
 from . import DMNotifyClient, generate_token, EmailError
 from .helpers import hash_string
 
 
-class UserAccountEmail():
+def send_user_account_email(role, email_address, template_id, extra_token_data={}, personalisation={}):
+    notify_client = DMNotifyClient(current_app.config['DM_NOTIFY_API_KEY'])
 
-    def __init__(self, token_data, template_id):
-        self.role = token_data['role']
-        self.email_address = token_data['email_address']
-        self.token = self._generate_create_token(token_data)
-        self.template_id = template_id
+    token_data = {
+        'role': role,
+        'email_address': email_address
+    }
+    token_data.update(extra_token_data)
 
-    def send_email(self, personalisation):
-        notify_client = DMNotifyClient(current_app.config['DM_NOTIFY_API_KEY'])
+    token = generate_token(
+        token_data,
+        current_app.config['SHARED_EMAIL_KEY'],
+        current_app.config['INVITE_EMAIL_SALT']
+    )
 
-        try:
-            notify_client.send_email(
-                self.email_address,
-                template_id=self.template_id,
-                personalisation=personalisation,
-                reference='create-user-account-{}'.format(hash_string(self.email_address))
-            )
-            session['email_sent_to'] = self.email_address
-        except EmailError as e:
-            current_app.logger.error(
-                "{code}: Create user email for email_hash {email_hash} failed to send. Error: {error}",
-                extra={
-                    'error': str(e),
-                    'email_hash': hash_string(self.email_address),
-                    'code': '{}create.fail'.format(self.role)
-                })
-            abort(503, response="Failed to send user creation email.")
+    link_url = url_for('external.create_user', encoded_token=token, _external=True)
 
-    def _generate_create_token(self, token_data):
-        return generate_token(
-            token_data,
-            current_app.config['SHARED_EMAIL_KEY'],
-            current_app.config['INVITE_EMAIL_SALT']
+    personalisation_with_link = personalisation.copy()
+    personalisation_with_link.update({'url': link_url})
+
+    try:
+        notify_client.send_email(
+            email_address,
+            template_id=template_id,
+            personalisation=personalisation_with_link,
+            reference='create-user-account-{}'.format(hash_string(email_address))
         )
+        session['email_sent_to'] = email_address
+    except EmailError as e:
+        current_app.logger.error(
+            "{code}: Create user email for email_hash {email_hash} failed to send. Error: {error}",
+            extra={
+                'error': str(e),
+                'email_hash': hash_string(email_address),
+                'code': '{}create.fail'.format(role)
+            })
+        abort(503, response="Failed to send user creation email.")
