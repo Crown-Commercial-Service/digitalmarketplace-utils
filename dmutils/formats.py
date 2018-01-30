f68e87b7fff5c880676042346d5b0ad6ce74cf00
Kev Keenoy <kevin.keenoy@digital.cabinet-office.gov.uk>
2018-01-30 10:55:40 +0000

    Add example formatted timestamps to formatter documentation

--- dmutils/formats.py
+++ dmutils/formats.py
@@ -13,6 +13,9 @@ DISPLAY_DATETIME_FORMAT = '%A %-d %B %Y at %I:%M%p %Z'
 
 def timeformat(value, default_value=None):
     """
+    Example value: datetime.strptime("2018-07-25 10:15:00", "%Y-%m-%d %H:%M:%S")
+    Example output: '11:15:00'
+
     "timeformat" is used in summary tables where space is tight and times are shown on their own line.
     Currently (Jan 2018) only used in admin app tables.
     """
@@ -21,6 +24,9 @@ def timeformat(value, default_value=None):
 
 def shortdateformat(value, default_value=None):
     """
+    Example value: datetime.strptime("2018-07-25 10:15:00", "%Y-%m-%d %H:%M:%S")
+    Example output: '25 July'
+
     "shortdateformat" was designed for use in summary tables where space is tight and dates are shown on their own line.
     The original intended use was in conjunction with "timeformat" in admin app summary tables.
     It is now (Jan 2018) also used in briefs-frontend on the "Publish your requirements" page only.
@@ -32,6 +38,9 @@ def shortdateformat(value, default_value=None):
 
 def dateformat(value, default_value=None):
     """
+    Example value: datetime.strptime("2018-07-25 10:15:00", "%Y-%m-%d %H:%M:%S")
+    Example output: 'Wednesday 25 July 2018'
+
     "dateformat" is the standard format used for dates in our frontend apps.
     If you're displaying a date on a page then this is probably the one you want.
     Examples of use:
@@ -44,6 +53,10 @@ def dateformat(value, default_value=None):
 
 def datetimeformat(value, default_value=None, localize=True):
     """
+    Example value: datetime.strptime("2018-07-25 23:59:59", "%Y-%m-%d %H:%M:%S")
+    Example output: 'Thursday 26 July 2018 at 12:59am BST'
+    Example output (localize=False): 'Wednesday 25 July 2018 at 11:59pm UTC'
+
     "datetimeformat" is the standard format used for timestamps (i.e. date and time) in our frontend apps.
     If you're displaying a date and time on a page then this is probably the one you want (unless it is a
     brief closing date, in which case use "utcdatetimeformat" below).
@@ -61,9 +74,14 @@ def datetimeformat(value, default_value=None, localize=True):
 
 def utcdatetimeformat(value, default_value=None):
     """
-    Only use this filter to format deadline timestamps that are stored in the database as
-    23:59:59 (UTC+00), when the localisation would otherwise display the date as 12.59am
-    on the next day (due to daylight savings), potentially causing confusion for buyers/suppliers.
+    Example value: datetime.strptime("2018-07-25 23:59:59", "%Y-%m-%d %H:%M:%S")
+    Example output: 'Wednesday 25 July 2018 at 11:59pm GMT'
+
+    "utcdatetimeformat" forces the timestamp to be output to be in GMT, no matter what the local time is.
+
+    This is intended *ONLY* to be used for *DEADLINES* that are stored in the database as 23:59:59 (UTC+00).
+    In this case localisation would display the date as 12.59am on the next day (due to daylight saving),
+    potentially causing confusion for buyers/suppliers.
     This is currently (Jan 2018) used wherever we show the closing date for briefs or brief clarification questions.
     """
     local_format = datetimeformat(value, default_value)
@@ -77,6 +95,9 @@ def utcdatetimeformat(value, default_value=None):
 
 def datetodatetimeformat(value):
     """
+    Example value: "2018-07-25"
+    Example output: 'Wednesday 25 July 2018'
+
     "datetodatetimeformat" is a convenience function to convert a date *STRING* from one format to another.
     This is currently (Jan 2018) only used to reformat the "startDate" from briefs, which is submitted by
     buyers in DATE_FORMAT but that we want to show to other users of the site in our standard DISPLAY_DATE_FORMAT.
