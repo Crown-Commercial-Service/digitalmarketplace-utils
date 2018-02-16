e2cfc8df0d4d73a55dc980330b99aad8ed5e5a48
George Lund <george.lund@digital.cabinet-office.gov.uk>
2018-02-16 17:53:13 +0000

    Shared access control function 'require_login'.
     - apologies will require weird partial application in order to be
       useful ;-)
     - done so that buyer and brief apps can share the same flash message.
    
    https://trello.com/c/lQIP4rf8/300-flash-messages-briefs-app

---
+++ dmutils/access_control.py
@@ -0,0 +1,24 @@
+from flask_login import current_user, login_required
+from flask import current_app, flash, Markup
+
+
+ROLES = {
+    'buyer': {
+        'loginRequiredMessage': Markup("You must log in with a buyer account to see this page."),
+    },
+}
+
+
+@login_required
+def require_login(role):
+    """
+    Shared function to limit access to a view by role. Intended to be used with Application.before_request
+    or Blueprint.before_request, for example:
+        some_blueprint.before_request(functools.partial(require_login, role='buyer'))
+    :param role: string, e.g. admin, buyer, supplier
+    :return: an unauthorized response, or None if access is allowed (once logged in)
+    """
+    if current_user.is_authenticated() and current_user.role != role:
+        flash(ROLES[role]['loginRequiredMessage'], 'error')
+        return current_app.login_manager.unauthorized()
+
