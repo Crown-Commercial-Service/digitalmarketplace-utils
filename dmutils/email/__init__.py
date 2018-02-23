aff86484a59ab087787fc3f0a96efbeff6dc140a
benvand <benjaminfvandersteen@gmail.com>
2018-02-23 13:46:13 +0000

    Remove backwards compatibility for email importing

--- dmutils/email/__init__.py
+++ dmutils/email/__init__.py
@@ -1,5 +1,3 @@
-"""Allow accessing send email from email. Backwards compatibility."""
-from dmutils.email.dm_mandrill import send_email
 from dmutils.email.tokens import (
     generate_token, decode_invitation_token, decode_password_reset_token
 )
