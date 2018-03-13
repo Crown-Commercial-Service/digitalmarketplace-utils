37869e27ad392de19a73158454f69ffd8d0c7e0e
Samuel Williams <Samuel.Williams@digital.cabinet-office.gov.uk>
2018-03-13 10:07:04 +0000

    Add disk space check for healthcheck
    We want to be able to check free disk space on instances as part of
    their healthcheck endpoint. Here's where we are going to do it.
    
    `get_disk_space_status` will report 'OK' if at least 5% disk space is
    free (by default) and 'LOW' if less than 5% is free.

---
+++ tests/test_status.py
@@ -0,0 +1,29 @@
+from collections import namedtuple
+
+import mock
+import pytest
+
+from dmutils.status import get_disk_space_status
+
+
+def statvfs_fixture(total_blocks, free_blocks):
+    statvfs = namedtuple('statvfs', 'f_blocks f_bfree')
+    return statvfs(total_blocks, free_blocks)
+
+
+@mock.patch('dmutils.status.os.statvfs')
+@pytest.mark.parametrize('kwargs, total_blocks, free_blocks, expected_status',
+                         (
+                             ({}, 1024, 0, ('OK', 100)),
+                             ({}, 1024, 512, ('OK', 50)),
+                             ({}, 1024, 920, ('OK', 10)),
+                             ({}, 1024, 1000, ('LOW', 2)),
+                             ({}, 1024, 1024, ('LOW', 0)),
+                             ({'low_disk_percent_threshold': 10}, 1024, 950, ('LOW', 7)),
+                             ({'low_disk_percent_threshold': 20}, 1024, 920, ('LOW', 10)),
+                             ({'low_disk_percent_threshold': 75}, 1024, 512, ('LOW', 50)),
+                         ))
+def test_disk_space_status(disk_usage, kwargs, total_blocks, free_blocks, expected_status):
+    disk_usage.return_value = statvfs_fixture(total_blocks, free_blocks)
+
+    assert get_disk_space_status(**kwargs) == expected_status
