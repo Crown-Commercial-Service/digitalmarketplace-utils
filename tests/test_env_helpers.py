75f92e499611c1caab297c79496cef810e26df4a
Kat Stevens <katherine.stevens@digital.cabinet-office.gov.uk>
2018-07-04 16:03:20 +0100

    Move env_helpers from scripts repo to utils
    Adds tests, and 'handles' the local case for the assets function
    by raising a NotImplementedError (as the local handling of static
    assets depends on the local nginx/router config).

---
+++ tests/test_env_helpers.py
@@ -0,0 +1,72 @@
+import pytest
+from dmutils.env_helpers import get_api_endpoint_from_stage, get_assets_endpoint_from_stage, get_web_url_from_stage
+
+
+class TestGetAPIEndpointFromStage:
+
+    @pytest.mark.parametrize('stage', ['local', 'dev', 'development'])
+    def test_get_api_endpoint_for_dev_environments(self, stage):
+        assert get_api_endpoint_from_stage(stage) == 'http://localhost:5000'
+
+    @pytest.mark.parametrize(
+        'stage, expected_result',
+        [
+            ('preview', 'https://api.preview.marketplace.team'),
+            ('staging', 'https://api.staging.marketplace.team'),
+            ('production', 'https://api.digitalmarketplace.service.gov.uk')
+        ]
+    )
+    def test_get_api_endpoint_for_non_dev_environments(self, stage, expected_result):
+        assert get_api_endpoint_from_stage(stage) == expected_result
+
+    @pytest.mark.parametrize('stage', ['local', 'dev', 'development'])
+    def test_get_search_api_endpoint_for_dev_environments(self, stage):
+        assert get_api_endpoint_from_stage(stage, app='search-api') == 'http://localhost:5001'
+
+    @pytest.mark.parametrize(
+        'stage, expected_result',
+        [
+            ('preview', 'https://search-api.preview.marketplace.team'),
+            ('staging', 'https://search-api.staging.marketplace.team'),
+            ('production', 'https://search-api.digitalmarketplace.service.gov.uk')
+        ]
+    )
+    def test_get_search_api_endpoint_for_non_dev_environments(self, stage, expected_result):
+        assert get_api_endpoint_from_stage(stage, app='search-api') == expected_result
+
+
+class TestGetWebUrlFromStage:
+
+    @pytest.mark.parametrize('stage', ['local', 'dev', 'development'])
+    def test_get_web_url_for_dev_environments(self, stage):
+        assert get_web_url_from_stage(stage) == 'http://localhost'
+
+    @pytest.mark.parametrize(
+        'stage, expected_result',
+        [
+            ('preview', 'https://www.preview.marketplace.team'),
+            ('staging', 'https://www.staging.marketplace.team'),
+            ('production', 'https://www.digitalmarketplace.service.gov.uk')
+        ]
+    )
+    def test_get_api_endpoint_for_non_dev_environments(self, stage, expected_result):
+        assert get_web_url_from_stage(stage) == expected_result
+
+
+class TestGetAssetsEndpointFromStage:
+
+    @pytest.mark.parametrize('stage', ['local', 'dev', 'development'])
+    def test_get_assets_endpoint_raises_error_for_dev_environments(self, stage):
+        with pytest.raises(NotImplementedError):
+            get_assets_endpoint_from_stage(stage)
+
+    @pytest.mark.parametrize(
+        'stage, expected_result',
+        [
+            ('preview', 'https://assets.preview.marketplace.team'),
+            ('staging', 'https://assets.staging.marketplace.team'),
+            ('production', 'https://assets.digitalmarketplace.service.gov.uk')
+        ]
+    )
+    def test_get_assets_endpoint_for_non_dev_environments(self, stage, expected_result):
+        assert get_assets_endpoint_from_stage(stage) == expected_result
