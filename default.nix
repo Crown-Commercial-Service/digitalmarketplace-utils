bddddf2919c82e95696f754055cc16bb69f8fb3d
Robert Scott <robert.scott@digital.cabinet-office.gov.uk>
2018-08-30 16:24:18 +0100

    default.nix: adapt to use of venv rather than virtualenv

--- default.nix
+++ default.nix
@@ -12,7 +12,7 @@ in (with args; {
     name = "digitalmarketplace-utils-frontend-env";
     shortName = "dm-utils";
     buildInputs = [
-      pythonPackages.virtualenv
+      pythonPackages.python
       pkgs.libffi
       pkgs.openssl
       pkgs.git
@@ -34,7 +34,7 @@ in (with args; {
       export PS1="\[\e[0;36m\](nix-shell\[\e[0m\]:\[\e[0;36m\]${shortName})\[\e[0;32m\]\u@\h\[\e[0m\]:\[\e[0m\]\[\e[0;36m\]\w\[\e[0m\]\$ "
 
       if [ ! -e $VIRTUALENV_ROOT ]; then
-        ${pythonPackages.virtualenv}/bin/virtualenv $VIRTUALENV_ROOT
+        ${pythonPackages.python}/bin/python -m venv $VIRTUALENV_ROOT
       fi
       source $VIRTUALENV_ROOT/bin/activate
       make requirements${pkgs.stdenv.lib.optionalString forDev "-dev"}
