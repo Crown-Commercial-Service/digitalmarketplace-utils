600e148b154684944ce818dbd2b937031505907b
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2018-09-14 12:57:59 +0100

    Pin monotonic requirement to 0.3 or greater

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
          'cryptography==2.3',
          'mailchimp3==2.0.11',
          'mandrill>=1.0.57',
-         'monotonic==0.3',
+         'monotonic>=0.3',
          'notifications-python-client<6.0.0,>=5.0.1',
          'odfpy==1.3.6',
          'python-json-logger==0.1.4',
