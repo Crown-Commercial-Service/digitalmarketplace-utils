669f26a8367a882f83e9d95c91c2aaa3aea27586
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2020-10-15 09:25:47 +0100

    Remove Flask-Script from dependencies
    It is deprecated, we no longer need it, let's get rid of it!

--- setup.py
+++ setup.py
@@ -23,7 +23,6 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-         'Flask-Script>=2.0.6',
          'Flask-WTF>=0.14.2',
          'Flask<1.1.0,>=1.0.2',
          'Flask-gzip>=0.2',
