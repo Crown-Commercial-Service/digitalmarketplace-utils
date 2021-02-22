7835c311e613d08fb514ee34e445c3444e13b648
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2021-02-22 16:01:24 +0000

    Add mypy testing to invoke tasks

--- tasks.py
+++ tasks.py
@@ -1,1 +1,12 @@
+from invoke import task
+
 from dmdevtools.invoke_tasks import library_tasks as ns
+
+
+@task(ns["virtualenv"], ns["requirements_dev"])
+def test_mypy(c):
+    c.run("mypy dmutils/")
+
+
+ns.add_task(test_mypy)
+ns["test"].pre.insert(-1, test_mypy)
