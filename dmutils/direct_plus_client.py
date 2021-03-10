7e4fa99a97384aa475a24fee0f99769d56748485
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2021-03-10 11:31:13 +0000

    Rewrite DirectPlusClient.get_organization_by_duns_number()
    We are having an issue where users can create supplier accounts with
    invalid DUNS numbers. This appears to be because although we expect the
    Direct+ error code `10001` for DUNS number not found, we are actually
    getting `40105`.
    
    This commit fixes this by changing the code to ignore the Direct+ error
    code and just look at the HTTP status code to determine whether the
    DUNS number is valid/found or not.
    
    Also we now:
    
    - log the HTTP status code along with the API response
    - always log a message if the HTTP status code is an error code
    - raise an exception instead of returning None (breaking change)
    - raise a custom exception if there is an error instead of raising
      ValueError or KeyError

--- dmutils/direct_plus_client.py
+++ dmutils/direct_plus_client.py
@@ -1,12 +1,21 @@
 import logging
-from typing import Optional, cast
+from typing import Union, cast
 
 import requests
 from requests import HTTPError
 
+
+class DirectPlusError(Exception):
+    pass
+
+
 # See https://directplus.documentation.dnb.com/errorsAndInformationMessages.html
-DUNS_NUMBER_NOT_FOUND = 404, "10001"
-DUNS_NUMBER_INVALID = 400, "10003"
+class DUNSNumberNotFound(DirectPlusError):
+    status_code = 404
+
+
+class DUNSNumberInvalid(DirectPlusError):
+    status_code = 400
 
 
 class DirectPlusClient(object):
@@ -78,15 +87,18 @@ class DirectPlusClient(object):
             )
         return response
 
-    def get_organization_by_duns_number(self, duns_number) -> Optional[dict]:
+    def get_organization_by_duns_number(self, duns_number: Union[str, int]) -> dict:
         """
-        Request a supplier by duns number from the Direct Plus API
+        Request a organization by D-U-N-S number from the Direct+ API
 
-        :return the organisation corresponding to the DUNS number; or `None` if the number is invalid or no
-                corresponding organisation exists.
-        :raises KeyError on unexpected failure if the response body is JSON.
-        :raises ValueError on unexpected failure if the response body is not valid JSON.
+        :return: the organization details corresponding to the DUNS number
+        :raises DUNSNumberNotFound: if there is no organization for the DUNS number
+        :raises DUNSNumberInvalid: if `duns_number` is not a valid DUNS number
+        :raises DirectPlusError: if there is an unexpected error response from the API
         """
+
+        # request the company profile, linkage, and executives (v2)
+        # https://directplus.documentation.dnb.com/openAPI.html?apiID=cmpelkv2
         response = self._direct_plus_request(
             f'data/duns/{duns_number}', payload={'productId': 'cmpelk', 'versionId': 'v2'}
         )
@@ -95,12 +107,34 @@ class DirectPlusClient(object):
             response.raise_for_status()
         except HTTPError as exception:
             try:
-                error = response.json()['error']
+                error = response.json()["error"]
+            except (ValueError, KeyError) as e:
+                error_message = f"Unable to parse Direct+ API response: {response}: {e}"
+                raise DirectPlusError(error_message, exception)
 
-                if (response.status_code, error["errorCode"]) in [DUNS_NUMBER_INVALID, DUNS_NUMBER_NOT_FOUND]:
-                    return None
+            error_message = (
+                f"Unable to get supplier for DUNS number '{duns_number}' (HTTP error {response.status_code}): {error}"
+            )
 
-                self.logger.error(f"Unable to get supplier by DUNS number: {error}")
-            except (ValueError, KeyError):
-                self.logger.error(f"Unable to get supplier by DUNS number: {exception}")
-        return cast(dict, response.json()['organization'])
+            if response.status_code in [DUNSNumberInvalid.status_code, DUNSNumberNotFound.status_code]:
+                self.logger.warning(error_message)
+
+                if response.status_code == DUNSNumberInvalid.status_code:
+                    raise DUNSNumberInvalid(error_message, exception)
+                if response.status_code == DUNSNumberNotFound.status_code:
+                    raise DUNSNumberNotFound(error_message, exception)
+
+            else:
+                self.logger.error(error_message)
+                raise DirectPlusError(error_message, exception)
+
+        try:
+            organization = cast(dict, response.json()['organization'])
+        except (ValueError, KeyError) as e:
+            # this should never happen, so let's propogate it so it is noisy
+            self.logger.error(
+                f"Unable to parse Direct+ API response: {response}: {e}"
+            )
+            raise
+
+        return organization
