9f75d888f6982dc4398acd18fc475df0e6c9748c
Benjamin Gill <benjamin.gill@crowncommercial.gov.uk>
2021-11-04 10:24:59 +0000

    Stop using custom www_authenticate handling logic
    Werkzeug added this functionality in 0.15.0. We're currently using Werkzeug 1.0.1. So we can safely retire our custom logic. The test shows that this does not change the bahaviour we rely on.
    
    This is needed to unblock the upgrade to Werkzeug 2.

--- dmutils/authentication.py
+++ dmutils/authentication.py
@@ -3,23 +3,10 @@ from werkzeug.exceptions import Unauthorized
 
 class UnauthorizedWWWAuthenticate(Unauthorized):
     """
-    This is a near verbatim copy of an improvement to an as-yet-unreleased upstream version of werkzeug that allows
-    us to specify a www_authenticate argument containing the contents of that field. We should get rid of this once
-    we're able to upgrade past that version.
-
-    From werkzeug 8ed5b3f9a285eca756c3ab33f8c370a88eab3842
+    This class exists for back-compatibility. We originally needed it to add support for `www_authenticate` before
+    werkzeug added it to Unauthorized. Users should switch to use Unauthorized directly.
     """
     def __init__(self, www_authenticate=None, description=None):
-        super().__init__(description=description)
         if not isinstance(www_authenticate, (tuple, list)):
             www_authenticate = (www_authenticate,)
-        self.www_authenticate = www_authenticate
-
-    def get_headers(self, environ=None):
-        headers = super().get_headers(environ)
-        if self.www_authenticate:
-            headers.append((
-                'WWW-Authenticate',
-                ', '.join([str(x) for x in self.www_authenticate])
-            ))
-        return headers
+        super().__init__(description=description, www_authenticate=www_authenticate)
