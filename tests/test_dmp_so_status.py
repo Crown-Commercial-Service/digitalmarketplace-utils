1ea67ff33a0d9a5ab14cb89fd7963ccd8d16d33a
tim-s-ccs <timothy.south@crowncommercial.gov.uk>
2022-01-05 14:28:22 +0000

    Change to make better sense semantically

--- tests/test_dmp_so_status.py
+++ tests/test_dmp_so_status.py
@@ -1,28 +1,28 @@
 from freezegun import freeze_time
 
-from dmutils.framework_notification import dos6_live
+from dmutils.dmp_so_status import are_new_frameworks_live
 
 
 @freeze_time('2022-01-04')
 def test_should_be_false_if_before_go_live_date():
-    assert dos6_live({}) is False
+    assert are_new_frameworks_live({}) is False
 
 
 @freeze_time('2022-01-14')
 def test_should_be_true_if_on_go_live_date():
-    assert dos6_live({}) is True
+    assert are_new_frameworks_live({}) is True
 
 
 @freeze_time('2022-01-15')
 def test_should_be_true_if_after_go_live_date():
-    assert dos6_live({}) is True
+    assert are_new_frameworks_live({}) is True
 
 
 @freeze_time('2022-01-04')
 def test_should_be_true_if_before_date_and_go_live_param():
-    assert dos6_live({'show_dos6_live': 'true'}) is True
+    assert are_new_frameworks_live({'show_dmp_so_banner': 'true'}) is True
 
 
 @freeze_time('2022-01-04')
 def test_should_be_false_if_before_date_and_not_go_live_param():
-    assert dos6_live({'show_gcloud13_live': 'true'}) is False
+    assert are_new_frameworks_live({'show_dmp_1.0_banner': 'true'}) is False
