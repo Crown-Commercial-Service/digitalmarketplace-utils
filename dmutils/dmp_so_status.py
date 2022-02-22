8560cf6074aed54ad5a42d96dceb7b6cdbaaa07a
Benjamin Gill <benjamin.gill@crowncommercial.gov.uk>
2022-02-22 12:11:21 +0000

    Hide DOS6 banner once it's closed
    We think it's best to remove the banner in this period. Telling users something is coming soon and giving a link that they cannot use might cause confusion. Sometimes giving information when a user wouldn't normally be looking for it (out of context) can raise more questions than answers.
    
    We'll need to rework this for G13 - I'm not doing that now because I don't know the equivalent dates for G13.

--- dmutils/dmp_so_status.py
+++ dmutils/dmp_so_status.py
@@ -1,7 +1,8 @@
-from datetime import date
+from datetime import datetime
 
-DMP_SO_GO_LIVE_DATE = '2022-01-14'
+DOS6_OPEN = datetime(2022, 1, 14, 12, 0, 0)
+DOS6_CLOSE = datetime(2022, 2, 24, 15, 0, 0)
 
 
 def are_new_frameworks_live(params) -> bool:
-    return date.today().strftime('%Y-%m-%d') >= DMP_SO_GO_LIVE_DATE or params.get('show_dmp_so_banner') == 'true'
+    return DOS6_OPEN <= datetime.now() <= DOS6_CLOSE or params.get('show_dmp_so_banner') == 'true'
--- tests/test_dmp_so_status.py
+++ tests/test_dmp_so_status.py
@@ -8,7 +8,7 @@ def test_should_be_false_if_before_go_live_date():
     assert are_new_frameworks_live({}) is False
 
 
-@freeze_time('2022-01-14')
+@freeze_time('2022-01-14 12:00:01')
 def test_should_be_true_if_on_go_live_date():
     assert are_new_frameworks_live({}) is True
 
@@ -25,4 +25,9 @@ def test_should_be_true_if_before_date_and_go_live_param():
 
 @freeze_time('2022-01-04')
 def test_should_be_false_if_before_date_and_not_go_live_param():
+    assert are_new_frameworks_live({'show_dmp_1.0_banner': 'true'}) is False
+
+
+@freeze_time('2022-02-24 15:00:01')
+def test_should_be_false_after_dos6_closes():
     assert are_new_frameworks_live({'show_dmp_1.0_banner': 'true'}) is False
