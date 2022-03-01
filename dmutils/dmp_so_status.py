30018711681f422dc8690f49dd277ad325487a38
tberey <thomas.berey@crowncommercial.gov.uk>
2022-03-01 17:44:41 +0000

    OST-509-Update-Banner-Datetimes-For-G13

--- dmutils/dmp_so_status.py
+++ dmutils/dmp_so_status.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 G13_OPEN = datetime(2022, 3, 9, 10, 0, 0)
-G13_CLOSE = datetime(2022, 5, 18, 15, 0, 0)
+G13_CLOSE = datetime(2022, 5, 18, 16, 0, 0)
 
 
 def are_new_frameworks_live(params) -> bool:
