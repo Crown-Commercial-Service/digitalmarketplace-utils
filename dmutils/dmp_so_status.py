6f2bc5ef58a588ad5d909ff0490465b140574efd
tberey <thomas.berey@crowncommercial.gov.uk>
2022-08-08 13:39:42 +0100

    Update Banner for new framework information and link to live dates. Always enable banner temporarily. Updates tests for enabled banner, when framework is not live

--- dmutils/dmp_so_status.py
+++ dmutils/dmp_so_status.py
@@ -4,5 +4,5 @@ G13_OPEN = datetime(2022, 3, 9, 10, 0, 0)
 G13_CLOSE = datetime(2022, 5, 18, 15, 0, 0)
 
 
-def are_new_frameworks_live(params) -> bool:
-    return G13_OPEN <= datetime.now() <= G13_CLOSE or params.get('show_dmp_so_banner') == 'true'
+def are_new_frameworks_live(_params) -> bool:
+    return True
